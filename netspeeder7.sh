0bc6e118c11f0cebe688fb138613cf7de6b6884e
pianha <pianha@gmail.com>
2017-02-13 00:36:53 +0800

    Update netspeeder7.sh

--- netspeeder7.sh
+++ netspeeder7.sh
@@ -22,4 +22,4 @@ NS_PATH=/usr/local/net_speeder
 mkdir -p $NS_PATH
 cp -Rf net_speeder $NS_PATH
 echo -e "\033[36m net_speeder installed. \033[0m"
-echo -e "\033[36m Usage: nohup ${NS_PATH}/net_speeder $INTERFACE \"ip\" >/dev/null 2>&1 & \033[0m;
+echo -e "\033[36m Usage: nohup ${NS_PATH}/net_speeder $INTERFACE \"ip\" >/dev/null 2>&1 & \033[0m;"
