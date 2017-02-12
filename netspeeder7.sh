201b19d5b563574a5b427a5d061de4ad84328eb0
pianha <pianha@gmail.com>
2017-02-13 00:31:37 +0800

    Fix a bug
    Fix line 29: syntax error: unexpected end of file

---
+++ netspeeder7.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Check If You Are Root
+if [ $(id -u) != "0" ]; then
+    clear
+    echo -e "\033[31m Error: You must be root to run this script! \033[0m"
+    exit 1
+fi
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
+rpm -ivh epel-release-7-8.noarch.rpm
+yum install -y libnet libnet-devel libpcap libpcap-devel gcc
+git clone https://github.com/snooda/net-speeder
+cd ~/net-speeder
+sudo chmod +x ./build.sh
+if [ -f /proc/user_beancounters ] || [ -d /proc/bc ]; then
+    sh build.sh -DCOOKED
+    INTERFACE=venet0
+else
+    sh build.sh
+    INTERFACE=eth0
+fi
+NS_PATH=/usr/local/net_speeder
+mkdir -p $NS_PATH
+cp -Rf net_speeder $NS_PATH
+echo -e "\033[36m net_speeder installed. \033[0m"
+echo -e "\033[36m Usage: nohup ${NS_PATH}/net_speeder $INTERFACE \"ip\" >/dev/null 2>&1 & \033[0m;
